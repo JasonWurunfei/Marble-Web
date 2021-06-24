21fc35be8248d177761d2a9ddbdba40a407679f6
LY <809822185@qq.com>
2021-06-25 00:03:35 +0800

    MarbleEdit-Initial Version(with some warns)

--- src/views/marble/MarbleEdit.vue
+++ src/views/marble/MarbleEdit.vue
@@ -1,14 +1,111 @@
 <template>
   <h1 v-if="id">Edit Marble {{ id }}</h1>
   <h2 v-else>Create Marble</h2>
+  <form @submit.prevent="createMarble">
+    <label>Name</label>
+    <input type="text" v-model="name" required>
+    
+    <label>Translation</label>
+    <input type="text" v-model="translation" required>
+    
+    <label>Story</label>
+    <input type="text" v-model="story">
+    
+    <label>Picture</label>
+    <input type="file">
+    
+    <label>Audio</label>
+    <input type="file">
+    
+    <label>Media</label>
+    <input type="file">
+
+    <button class="submit">Create</button> 
+  </form>
+  
 </template>
 
 <script>
+import { inject } from 'vue'
+import { useStore } from 'vuex'
+import { ref } from '@vue/reactivity'
+import checkLogin from "@/composables/checkLogin"
+
 export default {
-  props: ['id']
+  props: ['id'],
+  setup() {
+    return checkLogin(() => {
+      const store = useStore()
+      const name = ref('')
+      const userId = store.state.user.id
+      const story = ref('')
+      const translation = ref('')
+      const axios = inject("$axios")
+
+      const createMarble = () => {
+        const creationTime = Date.now()
+        axios({
+          method: 'post',
+          url: "marble/",
+          data: {
+            name: name.value,
+            userId: parseInt(userId),
+            translation: translation.value,
+            story: story.value
+          }
+        })
+      }
+      return { name, translation, story, createMarble }
+    })
+    
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+  form {
+      max-width: 30px 420px;
+      margin: 30px auto;
+      background: white;
+      text-align: left;
+      padding: 40px;
+      border-radius: 10px;
+  }
+  label {
+      color: #aaa;
+      display: inline-block;
+      margin: 25px 0 15px;
+      font-size: 0.6em;
+      text-transform: uppercase;
+      letter-spacing: 1px;
+      font-weight: bold;
+  }
+  input, select {
+      display: block;
+      padding: 10px 6px;
+      width: 100%;
+      box-sizing: border-box;
+      border: none;
+      border-bottom: 1px solid #ddd;
+      color: #555;
+  }
+  input[type='checkbox'] {
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
+  }
+  button {
+    background: #0b6dff;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+    cursor: pointer;
+  }
+  .submit {
+    text-align: center;
+  }
 </style>
