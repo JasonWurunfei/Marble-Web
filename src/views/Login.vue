352b46a39d8b1d7ca8820a190956c53a8efb07eb
JasonWu <47469275+JasonWurunfei@users.noreply.github.com>
2021-06-25 17:47:37 +0800

    finish login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,32 @@
 <template>
-  <input type="text" v-model="username" @keydown="enter">
-  <button @click="login">Login</button>
+  <div class="login">
+    <div class="login-form">
+      <h1 class="caption">请 登 录</h1>
+      <AnimationInputField
+        fieldName="username"
+        labelText="用户名："
+        v-model="username"
+        @keydown="enter"
+      />
+    </div>
+    <WhiteSquareBtn 
+      text="Login"
+      :icon='require("@/assets/login.svg")'
+      :callback="login" />
+  </div>
 </template>
 
 <script>
 import {useStore} from 'vuex'
+import AnimationInputField from "@/components/AnimationInputField.vue"
+import WhiteSquareBtn from "@/components/buttons/WhiteSquareBtn.vue"
 import {useRoute, useRouter} from 'vue-router'
 import { ref } from '@vue/reactivity'
 import { inject } from 'vue'
 
 
 export default {
+  components: {AnimationInputField, WhiteSquareBtn},
   setup() {
     const store = useStore()
     const route = useRoute()
@@ -51,5 +67,46 @@ export default {
 </script>
 
 <style>
+.login {
+  width: 100%;
+  height: 100%;
+  display: grid;
+  place-items: center;
+  grid-template-rows: 3fr 1fr;
+  position: relative;
+}
+.login::after {
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  background: url("~@/assets/login-background.jpg");
+  background-size: 100% 100%;
+  z-index: -2;
+}
+
+.login::before {
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  background: black;
+  opacity: 0.15;
+  background-size: 100% 100%;
+  z-index: -1;
+}
+
+.login-form {
+  width: 80%;
+}
+.login-form .caption {
+  text-align: center;
+  color: white;
+  font-size: 2rem;
+}
 
 </style>
