85a1a88995033f97fd24424c018342cde6ea539c
JasonWu <47469275+JasonWurunfei@users.noreply.github.com>
2021-06-28 15:50:37 +0800

    change base URL and finish data fetching for marble detail page

--- src/views/marble/MarbleDetail.vue
+++ src/views/marble/MarbleDetail.vue
@@ -8,6 +8,36 @@
     </div>
     <div class="impressions">
 
+      <section class="videos" >
+        <ul v-if="videos">
+          <li v-for="video in videos" :key="video.id">
+            <video width="320" height="240" controls>
+              <source :src="baseURL+video.path" >
+              Your browser does not support the video tag.
+            </video>
+          </li>
+        </ul>
+      </section>
+
+      <section class="images" >
+        <ul v-if="images">
+          <li v-for="image in images" :key="image.id">
+            <img :src="baseURL+image.path" alt="" width="200">
+          </li>
+        </ul>
+      </section>
+
+      <section class="audios" >
+        <ul v-if="audios">
+          <li v-for="audio in audios" :key="audio.id">
+            <audio controls>
+              <source :src="baseURL+audio.path" >
+              Your browser does not support the audio element.
+            </audio>
+          </li>
+        </ul>
+      </section>
+
     </div>
   </div>
 
@@ -21,13 +51,31 @@ export default {
   setup(props) {
 
     const marble = ref(null)
+    const videos = ref(null)
+    const images = ref(null)
+    const audios = ref(null)
     const axios = inject("$axios")
+    const baseURL = inject("baseURL")
 
-    axios.get(`marble/${props.id}`)
+    axios.get(`api/marble/${props.id}`)
     .then(res => marble.value = res.data)
     .catch(err => console.error(err.message))
 
-    return { marble }
+    axios.get(`api/impression/marble/${props.id}`)
+    .then(res => {
+      videos.value = res.data.filter(impression => impression.type === 1)
+      images.value = res.data.filter(impression => impression.type === 2)
+      audios.value = res.data.filter(impression => impression.type === 3)
+    })
+    .catch(err => console.error(err.message))
+
+    return { 
+      marble,
+      videos,
+      images,
+      audios,
+      baseURL
+    }
 
   }
 }
